6e7651d2cdea0fd60c4c7811123ae6213c924307
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2025-04-20 06:17:13 -0500

    completed until grounding llms

--- explore-rag/src/main/java/com/llm/service/GroundingService.java
+++ explore-rag/src/main/java/com/llm/service/GroundingService.java
@@ -30,10 +30,6 @@ import java.util.stream.Collectors;
 public class GroundingService {
 
     private static final Logger log = LoggerFactory.getLogger(GroundingService.class);
-    private final PgVectorStore vectorStore;
-
-
-    private String handbookContent;
 
     private final ChatClient chatClient;
 
@@ -44,61 +40,12 @@ public class GroundingService {
     @Value("classpath:/prompt-templates/RAG-QA-Prompt.st")
     private Resource ragQAPrompt;
 
-    public GroundingService(ChatClient.Builder chatClientBuilder,
-//                            PgVectorStore vectorStore
-                            @Qualifier(value = "qaVectorStore") PgVectorStore vectorStore
-                             ) {
+    public GroundingService(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
-        this.vectorStore = vectorStore;
     }
 
     public GroundingResponse grounding(GroundingRequest groundingRequest) {
-        log.info("Prompt : {} ", groundingRequest.prompt());
-        log.info("context : {} ", handbookContent);
-        PromptTemplate promptTemplate = new PromptTemplate(ragPrompt);
-        var prompMessage = promptTemplate.createMessage(Map.of("input", groundingRequest.prompt(),
-                "context", handbookContent));
-        var prompt = new Prompt(List.of(prompMessage));
-        var response = chatClient.prompt(prompt).call().content();
-        return new GroundingResponse(response);
-    }
-
-    public GroundingResponse retrieveAnswer(GroundingRequest groundingRequest) {
-
-//        var response = chatClient.prompt(groundingRequest.prompt()).call().content();
-
-        var results = vectorStore.doSimilaritySearch(SearchRequest.builder()
-                .query(groundingRequest.prompt())
-                .build());
-
-        log.info("results size : {} \n results  : {} ", results.size(), results);
-        var context = results
-                .stream()
-                .filter(Objects::nonNull)
-//                .filter(result -> result.getScore() > 0.8)
-                .limit(2)
-                .map(result -> result.getText())
-                .collect(Collectors.joining("\n"));
-        if (StringUtils.isNotEmpty(context)) {
-            log.info("Matched context : {} ", context);
-            PromptTemplate promptTemplate = new PromptTemplate(ragQAPrompt);
-            var prompMessage = promptTemplate.createMessage(Map.of("input", groundingRequest.prompt(),
-                    "context", context));
-            var prompt = new Prompt(List.of(prompMessage));
-            var response = chatClient.prompt(prompt).call().content();
-
-            return new GroundingResponse(response);
-        } else {
-            log.info("No relevant context, so sending default response");
-            return new GroundingResponse("Sorry No Relevant Info found");
-        }
-
-    }
-
-    @PostConstruct
-    public void init() throws IOException {
-        Path filePath = Paths.get("explore-rag/src/main/resources/docs/technova-handbook.txt");
-        handbookContent = Files.readString(filePath);
+       return null;
     }
 
 
