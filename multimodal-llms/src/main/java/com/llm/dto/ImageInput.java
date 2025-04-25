527a8f953aa2183ec212e6aea3efd4e9fc710f8a
Dilip.Sundarraj <dilip.sundarraj@gmail.com>
2025-04-24 20:58:36 -0500

    added the code for the image generation content

--- multimodal-llms/src/main/java/com/llm/dto/ImageInput.java
+++ multimodal-llms/src/main/java/com/llm/dto/ImageInput.java
@@ -2,9 +2,9 @@ package com.llm.dto;
 
 
 public record ImageInput(
+        String prompt,
         String model,
         String quality,
-        String prompt,
         int height,
         int width,
         String style,
