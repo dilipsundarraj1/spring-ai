174804b55853ef5c54198458c6126578bd40abae
Dilip Sundarraj <dilip.bandlasundarraj@gmail.com>
2025-05-11 05:18:47 -0500

    removed date time tools

--- explore-openai/src/main/java/com/llm/tool_calling/ToolCallingController.java
+++ explore-openai/src/main/java/com/llm/tool_calling/ToolCallingController.java
@@ -1,11 +1,6 @@
 package com.llm.tool_calling;
 
 import com.llm.dto.UserInput;
-import com.llm.tool_calling.currency.CurrencyTools;
-import com.llm.tool_calling.currenttime.DateTimeTools;
-import com.llm.tool_calling.weather.WeatherConfigProperties;
-import com.llm.tool_calling.weather.WeatherToolsFunction;
-import com.llm.tool_calling.weather.dtos.WeatherRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
@@ -16,16 +11,7 @@ import org.springframework.ai.model.tool.ToolCallingChatOptions;
 import org.springframework.ai.model.tool.ToolCallingManager;
 import org.springframework.ai.model.tool.ToolExecutionResult;
 import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.ai.tool.ToolCallback;
-import org.springframework.ai.tool.ToolCallbacks;
-import org.springframework.ai.tool.definition.ToolDefinition;
-import org.springframework.ai.tool.function.FunctionToolCallback;
-import org.springframework.ai.tool.method.MethodToolCallback;
-import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
-
-import java.lang.reflect.Method;
-import java.util.Map;
 
 @RestController
 public class ToolCallingController {
@@ -57,11 +43,11 @@ public class ToolCallingController {
     @PostMapping("/v2/tool_calling/custom")
     public ChatResponse toolCallingCustom(@RequestBody UserInput userInput) {
 
-        ToolCallback[] tools = ToolCallbacks.from(new DateTimeTools());
+//        ToolCallback[] tools = ToolCallbacks.from(new DateTimeTools());
         ToolCallingManager toolCallingManager = ToolCallingManager.builder().build();
 
         ChatOptions chatOptions = ToolCallingChatOptions.builder()
-                .toolCallbacks(tools)
+//                .toolCallbacks(tools)
                 .internalToolExecutionEnabled(false)
                 .build();
         Prompt prompt = new Prompt(userInput.prompt(), chatOptions);
