f01b4f8e9693f6ab4121a2e495588d78712a5ab6
Dilip Sundarraj <dilip.bandlasundarraj@gmail.com>
2025-05-27 05:08:52 -0500

    updated the code for the main branch

--- multimodal-llms/src/main/java/com/llm/image/ImageController.java
+++ multimodal-llms/src/main/java/com/llm/image/ImageController.java
@@ -2,13 +2,14 @@ package com.llm.image;
 
 import com.llm.dto.ImageInput;
 import com.llm.dto.UserInput;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
-import org.springframework.util.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,60 +24,10 @@ import static com.llm.utils.ImageUtil.saveImageToFile;
 public class ImageController {
     private static final Logger log = LoggerFactory.getLogger(ImageController.class);
     public OpenAiImageModel openAiImageModel;
+
     public ImageController(OpenAiImageModel openAiImageModel) {
         this.openAiImageModel = openAiImageModel;
     }
 
 
-    @PostMapping("/v1/images")
-    public ImageResponse images(@RequestBody UserInput userInput){
-        log.info("userInput message prompt is : {} ", userInput);
-        var response = openAiImageModel.call(
-                new ImagePrompt(userInput.prompt())
-
-        );
-        log.info("response : {} ", response.getResult().getOutput());
-        return  response;
-    }
-
-    @PostMapping("/v2/images")
-    public ImageResponse chat(@RequestBody ImageInput imageInput){
-        log.info("imageInput message prompt is : {} ", imageInput);
-        var quality = imageInput.quality()==null ? "hd" : imageInput.quality();
-        var height = imageInput.height()==0 ? 1024 : imageInput.height();
-        var width = imageInput.width()==0 ? 1024 : imageInput.width();
-        var model = !StringUtils.hasText(imageInput.model())  ? "dall-e-3" : imageInput.model();
-        var style = !StringUtils.hasText(imageInput.style())  ? "natural" : imageInput.style();
-        var responseFormat = !StringUtils.hasText(imageInput.responseFormat())  ? "url" : imageInput.responseFormat();
-        log.info("quality : {} , height : {} , width: {} , model : {} , style : {} ", quality, height, width, model, style);
-        var response = openAiImageModel.call(
-                new ImagePrompt(imageInput.prompt(),
-                        OpenAiImageOptions.builder()
-                                // Model : dall-e-3 or dall-e-2
-                                .model(model)
-                                //hd or standard
-                                .quality(quality)
-                                // The number of images to generate.
-                                // Must be between 1 and 10. For dall-e-3, only n=1 is supported.
-                                // Must be one of 256x256, 512x512, or 1024x1024 for dall-e-2
-                                // Must be one of 1024x1024, 1792x1024, or 1024x1792 for dall-e-3 models.
-                                .height(height)
-                                .width(width)
-                                //This property only works for dall-e-3 model.
-                                .style(style)
-                                .responseFormat(responseFormat)
-                                .build())
-
-        );
-        //log.info("response : {} ", response.getResult().getOutput());
-        var image = decodeBase64ToImage(response.getResult().getOutput().getB64Json());
-        String filePath = "output_image.png"; // specify the desired file path and format
-        boolean success = saveImageToFile(image, "png", filePath);
-        if (success) {
-            log.info("Image successfully parsed and saved as '" + filePath + "'!");
-        } else {
-            log.info("Failed to save the image.");
-        }
-        return  response;
-    }
 }
--- multimodal-llms/src/main/java/com/llm/vision/VisionController.java
+++ multimodal-llms/src/main/java/com/llm/vision/VisionController.java
@@ -4,19 +4,20 @@ import com.llm.dto.UserInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.ChatModelCallAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.content.Media;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @RestController
 public class VisionController {
@@ -28,43 +29,8 @@ public class VisionController {
 
 
     public VisionController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
-    }
 
-    @PostMapping("/v1/vision")
-    public String vision(@RequestBody UserInput userInput) {
-        log.info("userInput message : {} ", userInput);
-        var imageResource = new ClassPathResource("files/vision/zebra.jpg");
-        var userMessage = new UserMessage(
-                userInput.prompt()); // media
-
-
-        var response = chatClient.prompt(new Prompt(userMessage)).call();
-        log.info("response : {} ", response);
-        return response.content();
-    }
-
-    @PostMapping(value = "/v2/vision", consumes = "multipart/form-data")
-    public ResponseEntity<String> visionV2(
-            @RequestParam("file") MultipartFile file,
-            @RequestParam("prompt") String prompt
-    ) {
-
-        try {
-            // Get the original filename
-            String fileName = file.getOriginalFilename();
-            log.info("Uploaded File name is :  {} " , fileName);
-
-            // Create UserMessage
-            var userMessage = new UserMessage(
-                    //"Explain what do you see in this picture?", // content
-                    prompt); // media
-            var response = chatClient.prompt(new Prompt(userMessage)).call();
-            log.info("response : {} ", response.chatResponse());
-            return ResponseEntity.ok( response.content());
-        } catch (Exception e) {
-            log.error("Exception is : {} ", e.getMessage(), e);
-            return ResponseEntity.status(500).body("File upload failed");
-        }
+        this.chatClient = chatClientBuilder
+                .build();
     }
 }
